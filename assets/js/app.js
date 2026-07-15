ba6bb985bdcb5f5700a415d67f525aabd769c593
Nikolai O. <nik@o84.ru>
2026-07-15 16:36:42 +0300

    feat: add register procedure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,16 @@
+class Player {
+  constructor(id, name, avatar, wins, loses) {
+    this.id = id;
+    this.name = name;
+    this.avatar = avatar;
+    this.wins = wins;
+    this.loses = loses;
+  }
+}
+
+let player;
+let title = "Not Fight Club";
+
 // create container
 const body = document.getElementsByTagName("body")[0];
 const main = body.appendChild(document.createElement("main"));
@@ -5,10 +18,28 @@ main.classList.add("main");
 const mainContainer = main.appendChild(document.createElement("div"));
 mainContainer.classList.add("container");
 
-registerForm();
+registerCheck();
+
+// check registration
+function registerCheck() {
+  let keys = Object.keys(localStorage);
+  for (let key of keys) {
+    let value = localStorage.getItem(key);
+    const keyHasNumber = /\d/.test(key);
+    if (keyHasNumber === true) {
+      player = JSON.parse(localStorage.getItem(key));
+    }
+  }
+  if (player === undefined) {
+    registerForm();
+  } else {
+    home();
+  }
+}
 
 // registration form
 function registerForm() {
+  document.title = title + " - Registration";
   let registerContaner = mainContainer.appendChild(document.createElement("div"));
   registerContaner.classList.add("register-container");
 
@@ -43,5 +74,16 @@ function registerForm() {
 
 // registration
 function register() {
-  console.log("register");
+  let playerName = document.getElementById("registerName").value.trim();
+  let playerId = Math.floor(Math.random() * 10000);
+  const player = new Player(playerId, playerName, "default", 0, 0);
+  localStorage[playerId] = JSON.stringify(player);
+  mainContainer.replaceChildren();
+  registerCheck();
+}
+
+// home
+function home() {
+  document.title = title + " - Home";
+  console.log("id=", player.id, "name=", player.name);
 }
